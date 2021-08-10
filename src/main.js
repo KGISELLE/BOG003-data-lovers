3692ec248f0e0743299b0dd95dc67b3ec874ecc1
Giselle <kgiselle17@gmail.com>
2021-08-10 10:41:37 -0500

    insertar poster  traido de data.js a tarjeta

--- src/main.js
+++ src/main.js
@@ -6,11 +6,51 @@ let allMovies = data.films;
 
 
 for (let i=0; i<allMovies.length;i++){
-    let screenPoster= document.createElement("IMG");
+    //-- Creando elemento "IMG" para extraer el Poster de la data --//
+    const screenPoster= document.createElement("IMG");
+    screenPoster.setAttribute("src", allMovies[i].poster);
+
+    //-- Creando elemento "TITLE" para extraer el titulo de la data --//
+    const titlePoster= document.createElement("h5");
+    titlePoster.innerHTML = allMovies[i].title;
+    // titlePoster.createTextNode("src", allMovies[i].title);//
+    /* titlePoster.textContent = e.title; */
+    
+
+
+    //-- Creando el divContainer padre "card-container" --//
+    let divContainer = document.createElement("div");
+    divContainer.setAttribute("class", "card-container");
+    
+    //-- Creando divPoster "front-card" div hijo de divContainer "card-container" --//
+    let divPoster = document.createElement("div");
+    divPoster.setAttribute("class", "front-card");
+    //-- mostrar Poster dentro de divPoster "front-card" --//
+    divPoster.appendChild(screenPoster);
+
+    //-- mostrar titulo de pelicula dentro de "front-card"
+    divPoster.appendChild(titlePoster);
+
+    
+    
+    //-- Creando divBack "back-card" div hijo de divContainer "card-container" --//
+    let divBack = document.createElement("div");
+    divBack.setAttribute("class", "back-card");
+    divBack.innerHTML = ""
+
+    //-- vinculando div padre "divConatiner" con sus div hijos "divPoster" y "divBack" --//
+    divContainer.appendChild(divPoster, divBack);
+
+    
     //let titleofMovie=document.createElement("TITLE");
     //titleofMovie=allMovies[i].title;
-    screenPoster.setAttribute("src", allMovies[i].poster);
-    document.querySelector(".posters").appendChild(screenPoster);  
+    document.querySelector(".posters").appendChild(divContainer);  
+
+    //-- Funcionalidad para girar la tarjeta --//
+    
+    divContainer.addEventListener("click", () => {
+    divContainer.classList.toggle("active")
+});
       
 }
 
