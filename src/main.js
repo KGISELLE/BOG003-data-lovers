a125bdff415a220a6ef24884a6eb8a9651e640a2
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-08-10 11:55:02 -0500

    depuración codigo main.js

--- src/main.js
+++ src/main.js
@@ -5,23 +5,23 @@ let allMovies = data.films;
 //console.log(allMovies[0].poster);
 
 
-for (let i=0; i<allMovies.length;i++){
+for (let i = 0; i < allMovies.length; i++) {
     //-- Creando elemento "IMG" para extraer el Poster de la data --//
-    const screenPoster= document.createElement("IMG");
+    const screenPoster = document.createElement("IMG");
     screenPoster.setAttribute("src", allMovies[i].poster);
 
     //-- Creando elemento "TITLE" para extraer el titulo de la data --//
-    const titlePoster= document.createElement("h5");
+    const titlePoster = document.createElement("h5");
     titlePoster.innerHTML = allMovies[i].title;
     // titlePoster.createTextNode("src", allMovies[i].title);//
     /* titlePoster.textContent = e.title; */
-    
+
 
 
     //-- Creando el divContainer padre "card-container" --//
     let divContainer = document.createElement("div");
     divContainer.setAttribute("class", "card-container");
-    
+
     //-- Creando divPoster "front-card" div hijo de divContainer "card-container" --//
     let divPoster = document.createElement("div");
     divPoster.setAttribute("class", "front-card");
@@ -31,8 +31,8 @@ for (let i=0; i<allMovies.length;i++){
     //-- mostrar titulo de pelicula dentro de "front-card"
     divPoster.appendChild(titlePoster);
 
-    
-    
+
+
     //-- Creando divBack "back-card" div hijo de divContainer "card-container" --//
     let divBack = document.createElement("div");
     divBack.setAttribute("class", "back-card");
@@ -41,63 +41,20 @@ for (let i=0; i<allMovies.length;i++){
     //-- vinculando div padre "divConatiner" con sus div hijos "divPoster" y "divBack" --//
     divContainer.appendChild(divPoster, divBack);
 
-    
+
     //let titleofMovie=document.createElement("TITLE");
     //titleofMovie=allMovies[i].title;
-    document.querySelector(".posters").appendChild(divContainer);  
+    document.querySelector(".posters").appendChild(divContainer);
 
     //-- Funcionalidad para girar la tarjeta --//
-    
+
     divContainer.addEventListener("click", () => {
-    divContainer.classList.toggle("active")
-});
-      
+        divContainer.classList.toggle("active")
+    });
+
 }
 
 
-
-
-
-//document.body.appendChild(screenPoster);
-
-//console.log(moviePoster);
-
-
-
-for (let item = 0; item < allMovies.length; item++) {
-    console.log(allMovies[item].title);
-    console.log()
-}
- //innerhtml
-//createlement
- //setatribute
-
-
-
-
-
-
-
-/* let imageObj = new Image();
-imageObj.src =allMovies.poster;
-//context.drawImage( imageObj, 0, 0 ); */
-
-//console.log(allMovies);
-
-/* let arrayData=data;
-console
-foreach(arrayData){
-
-} */
-
-//console.log(example, data);
-
-
-//-- Funcionalidad para girar la tarjeta --//
-const card = document.querySelector(".card-container");
-card.addEventListener("click", () => {
-    card.classList.toggle("active")
-});
 
 
 
